01aaef46e5c137be17e615f612679b39ddf3e7b8
Nama Kamu <nafidznafidz478@gmail.com>
2026-07-20 15:38:10 +0800

    studikasus8

---
+++ studikasus8.js
@@ -0,0 +1,15 @@
+const menu = [
+10000,
+25000,
+18000,
+12000,
+50000
+];
+
+for (let i = 0; i < menu.length; i++) {
+if (menu[i] > 20000) {
+    console.log("Menu " + (i + 1) + " : Rp" + menu[i] + " - Menu Premium");
+} else {
+    console.log("Menu " + (i + 1) + " : Rp" + menu[i] + " - Menu Reguler");
+}
+}
